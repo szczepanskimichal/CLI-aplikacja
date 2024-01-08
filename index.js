66032312d75a7f2dd8c1abac35d0c28c7694a79a
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-01-08 13:44:50 +0100

    correct definition of scripts, commander instalation, working with index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const { Command } = require("commander");
+
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts.js");
+
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+program.parse(process.argv);
+
+const argv = program.opts();
+console.log(__dirname);
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contactById = await getContactById(id);
+      console.log(contactById);
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const contact = await removeContact(id);
+      console.log(contact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
